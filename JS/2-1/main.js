3aebe9e01b1432a1af86dc06a3f440edcd625ed2
舘山廉 <rennex175@gmail.com>
2020-08-12 20:28:47 +0900

    JS2章課題提出
    JS２章の課題ができたので提出します。

---
+++ JS/2-1/main.js
@@ -0,0 +1,10 @@
+// document.body.textContent = 'hello';
+// document.title = 'js2-1';
+// console.log(window);
+
+// let url = window.location.href;
+
+// console.log(url);
+
+// window.alert('hello world');
+// console.log('aaa');
