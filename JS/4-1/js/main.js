a7e27c54379f8ff3c46409a1cdfdf3dd13ccc5f1
舘山廉 <rennex175@gmail.com>
2020-08-18 00:44:47 +0900

    js4章課題提出
    js4章のチェックテストの作成

---
+++ JS/4-1/js/main.js
@@ -0,0 +1,56 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        list: [],
+        addText: '',
+        keyword: '',
+    },
+    //watchでlistの変更を監視
+    watch: {
+        list: {
+            handler: function() {
+                //localStorageにデータを保存
+                localStorage.setItem("list", JSON.stringify(this.list));
+            },
+            deep: true
+        }
+    },
+    //マウントされた時にlocalStorageからデータを取得
+    mounted: function() {
+        this.list = JSON.parse(localStorage.getItem("list")) || [];
+    },
+    methods: {
+        addToDo: function() {
+            if (this.addText !== '') {
+                this.list.push({
+                    text: this.addText, 
+                    isChecked: false,
+                });
+            }
+            this.addText = '';
+        },
+        deleteBtn: function() {
+            this.list = this.list.filter(function(todo) {
+                return !todo.isChecked;
+            });
+        },
+    },
+    computed: {
+        remaining: function(){
+            var items = this.list.filter(function(list){
+                return !list.isChecked;
+            });
+            return items.length;
+        },
+        filteredTasks: function() {
+            var tasks = [];
+            for(var i in this.list) {
+                var task = this.list[i];
+                if(task.text.indexOf(this.keyword) !== -1) {
+                    tasks.push(task);
+                }
+            }
+            return tasks;
+        }
+    },
+});
